9f859a4e1150d09304ece401fad4fb6a6c4df99b
cyzhzhd <cyzhzhd@gmail.com>
2025-03-30 22:01:54 +0900

    [feat] test 케이스 추가

--- internal/infrastructure/persistence/brands/brand_adapter.go
+++ internal/infrastructure/persistence/brands/brand_adapter.go
@@ -3,7 +3,6 @@ package brands
 import (
 	appBrands "dresser/internal/application/brands"
 	"dresser/internal/domain/brands"
-	"dresser/internal/domain/categoriess"
 )
 
 // BrandAdapter converts between domain and application brand models
@@ -15,11 +14,7 @@ func NewBrandAdapter() *BrandAdapter {
 
 // ToDomain converts application brand to domain brand
 func (a *BrandAdapter) ToDomain(appBrand *appBrands.BrandDTO) (*brands.Brand, error) {
-	categories, err := categoriess.NewCategories(appBrand.Categories)
-	if err != nil {
-		return nil, err
-	}
-	return brands.New(appBrand.ID, appBrand.Name, *categories)
+	return brands.New(appBrand.ID, appBrand.Name)
 }
 
 // ToApplication converts domain brand to application brand
